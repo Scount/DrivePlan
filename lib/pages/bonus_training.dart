1a5930a623d51988cf895f221a96b84acfff40c4
Raptus <lee@familiehartwich.de>
2020-11-28 15:54:38 +0100

    finished bonus training

--- lib/pages/bonus_training.dart
+++ lib/pages/bonus_training.dart
@@ -8,54 +8,78 @@ class BonusTraining extends StatefulWidget {
 }
 
 class _BonusTrainingState extends State<BonusTraining> {
+  var dropDownValue = "Praxispakete";
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
         length: 2,
         child: Scaffold(
-          appBar: AppBar(
-            title: Text("Bonus Training"),
-            bottom: TabBar(tabs: [
-              Tab(text: "Theorie",),
-              Tab(text: "Praxis",),
-            ],),
-          ),
-          body: TabBarView(
-            children: [
-              theorieBody(),
-              praxisBody()
-            ],)
-        ));
+            appBar: AppBar(
+              title: Text("Bonus Training"),
+              bottom: TabBar(
+                tabs: [
+                  Tab(
+                    text: "Theorie",
+                  ),
+                  Tab(
+                    text: "Praxis",
+                  ),
+                ],
+              ),
+            ),
+            body: TabBarView(
+              children: [theorieBody(), praxisBody()],
+            )));
   }
-  Widget praxisBody(){
+
+  Widget praxisBody() {
     return Container(
       child: Column(
         children: [
-          praxisCard("Praxispakete"),
-          praxisCard("Wahrnehmungsbilder"),
-          praxisCard("Suchbilder")
+          DropdownButton(
+            value: dropDownValue,
+            items: <String>["Praxispakete", "Wahrnehmungsbilder", "Suchbilder"]
+                .map((e) => DropdownMenuItem(
+                      child: Text(e),
+                      value: e,
+                    ))
+                .toList(),
+            onChanged: (value) {
+              setState(() {
+                dropDownValue = value;
+              });
+            },
+          ),
+          dropDownValue == "Praxispakete" ? praxisPaketeWidget() : (dropDownValue == "Wahrnehmungsbilder" ? wahrnehmungsBilderWidget() : suchbilderWidget())
         ],
-      ),);
-  }
-  Widget praxisCard(String title){
-    return GestureDetector(
-      child: Card(
-        child: Center(child:Text(title)),
       ),
     );
   }
-  Widget theorieBody(){
+
+  Widget praxisPaketeWidget() {
+    return Container(child: Center(child: Text("Praxis"),),);
+  }
+  Widget wahrnehmungsBilderWidget() {
+    return Container(child: Center(child: Text("Wahrnehmung"),),);
+  }
+  Widget suchbilderWidget() {
+    return Container(child: Center(child: Text("Such"),),);
+  }
+
+  Widget theorieBody() {
     return ListView.builder(
       itemCount: 10,
-      itemBuilder: (context,index) => theorieCard(index),
+      itemBuilder: (context, index) => theorieCard(index),
       shrinkWrap: true,
-
     );
   }
-  Widget theorieCard(int index){
-    return Card(child: ListTile(
+
+  Widget theorieCard(int index) {
+    return Card(
+        child: ListTile(
       title: Text(index.toString()),
       subtitle: Text("Nico stinkt"),
-      trailing: Icon(Icons.arrow_forward_ios),));
+      trailing: Icon(Icons.arrow_forward_ios),
+    ));
   }
 }
