c43f8a7c762d61e369e0fb5cb31dd4415f44b764
Raptus <lee@familiehartwich.de>
2020-12-29 13:36:45 +0100

    implemented statitic page

--- lib/pages/statistik.dart
+++ lib/pages/statistik.dart
@@ -1,3 +1,4 @@
+import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 
 class Statistik extends StatefulWidget {
@@ -26,16 +27,170 @@ class _StatistikState extends State<Statistik> {
               ],
             ),
           ),
-          body: TabBarView(children: [
-            fragenStatistik(),
-            pruefungsSimulationen()
-          ]),
+          body: TabBarView(
+              children: [fragenStatistik(), pruefungsSimulationen()]),
         ));
   }
-  Widget fragenStatistik(){
-    return Container();
+
+  Widget fragenStatistik() {
+    return Column(
+      children: [
+        Expanded(child: fragenStatistikChart()),
+        Expanded(child: fragenStatistikBody())
+      ],
+    );
   }
-  Widget pruefungsSimulationen(){
-    return Container();
+
+  Widget fragenStatistikChart() {
+    return Card(
+      child: BarChart(BarChartData(
+          alignment: BarChartAlignment.spaceAround,
+          maxY: 100,
+          titlesData: FlTitlesData(
+            bottomTitles: SideTitles(
+              showTitles: true,
+              margin: 20,
+              getTitles: (double value) {
+                switch (value.toInt()) {
+                  case 0:
+                    return "Richtig";
+                  case 1:
+                    return "Falsch";
+                  case 2:
+                    return "Ungelöst";
+                  default:
+                    return "error";
+                }
+              },
+            ),
+            topTitles: SideTitles(
+              showTitles: true,
+              margin: 20,
+              getTitles: (double value) {
+                switch (value.toInt()) {
+                  case 0:
+                    return "20";
+                  case 1:
+                    return "10";
+                  case 2:
+                    return "100";
+                  default:
+                    return "error";
+                }
+              },
+            ),
+            leftTitles: SideTitles(showTitles: false),
+            show: true,
+          ),
+          borderData: FlBorderData(show: false),
+          barGroups: [
+            BarChartGroupData(
+              x: 0,
+              barRods: [
+                BarChartRodData(
+                  y: 20,
+                  width: 20,
+                  colors: [Colors.green],
+                  backDrawRodData: BackgroundBarChartRodData(
+                      show: true, y: 100, colors: [Colors.grey[200]]),
+                  borderRadius: BorderRadius.circular(1),
+                )
+              ],
+            ),
+            BarChartGroupData(
+              x: 1,
+              barRods: [
+                BarChartRodData(
+                  y: 10,
+                  width: 20,
+                  colors: [Colors.red],
+                  backDrawRodData: BackgroundBarChartRodData(
+                      show: true, y: 100, colors: [Colors.grey[200]]),
+                  borderRadius: BorderRadius.circular(1),
+                )
+              ],
+            ),
+            BarChartGroupData(
+              x: 2,
+              barRods: [
+                BarChartRodData(
+                  y: 80,
+                  width: 20,
+                  colors: [Colors.grey],
+                  backDrawRodData: BackgroundBarChartRodData(
+                      show: true, y: 100, colors: [Colors.grey[200]]),
+                  borderRadius: BorderRadius.circular(1),
+                )
+              ],
+            ),
+          ])),
+    );
+  }
+
+  Widget fragenStatistikBody() {
+    return Card(
+      child: Column(
+        children: [
+          Text("Fragenstatistik"),
+          Divider(),
+          Expanded(
+              child: SingleChildScrollView(
+            child: Column(
+              children: [
+                CheckboxListTile(
+                    title: Text("Alle Themengebiete auswerten"),
+                    value: true,
+                    onChanged: (value) {}),
+                ListTile(
+                  title: Text("Themengebiete Grundwissen"),
+                  tileColor: Colors.grey,
+                ),
+                CheckboxListTile(
+                    title: Text("Test"), value: true, onChanged: (value) {}),
+                CheckboxListTile(
+                    title: Text("Test"), value: true, onChanged: (value) {}),
+                CheckboxListTile(
+                    title: Text("Test"), value: true, onChanged: (value) {}),
+                CheckboxListTile(
+                    title: Text("Test"), value: true, onChanged: (value) {}),
+                CheckboxListTile(
+                    title: Text("Test"), value: true, onChanged: (value) {}),
+                CheckboxListTile(
+                    title: Text("Test"), value: true, onChanged: (value) {}),
+                ListTile(
+                  title: Text("Themengebiete Motorrad"),
+                  tileColor: Colors.grey,
+                ),
+                CheckboxListTile(
+                    title: Text("Test"), value: true, onChanged: (value) {}),
+              ],
+            ),
+          ))
+        ],
+      ),
+    );
+  }
+
+  Widget pruefungsSimulationen() {
+    return Card(child: Column(children: [
+      Text("Prüfungssimultationen"),
+      DataTable(
+        columnSpacing: 20,
+        headingRowColor: MaterialStateProperty.resolveWith<Color>((states) => Colors.grey),
+        columns: const <DataColumn>[
+          DataColumn(label: Text("Datum",style: TextStyle(fontStyle: FontStyle.italic),)),
+          DataColumn(label: Text("Klasse",style: TextStyle(fontStyle: FontStyle.italic),)),
+          DataColumn(label: Text("Fehlerpunkte",style: TextStyle(fontStyle: FontStyle.italic),)),
+          DataColumn(label: Text("Ergebnis",style: TextStyle(fontStyle: FontStyle.italic),)),
+        ], 
+        rows: const <DataRow>[
+          DataRow(cells: <DataCell>[
+            DataCell(Text("12.12.2020")),
+            DataCell(Text("B")),
+            DataCell(Text("42")),
+            DataCell(Text("Bestanden")),
+          ])
+        ])
+    ],),);
   }
 }
